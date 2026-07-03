ffd0d16d493e4ed743012081c8312aafc5a0b3a1
Armin Fuchs <22221011+PowerNow@users.noreply.github.com>
2026-07-03 15:54:52 +0200

    chore: Optimize screenshot

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,26 @@
 import { App, cfg } from 'illutions';
 
-// Configures the cameras position.
-cfg.cam.position = { x: 0, y: 1, z: 3 }
+// Set cameras position
+cfg.cam.position = { x: -2.25, y: 0.25, z: 0 }
 
-// Configures how the camera rotates, pans and zooms around a target.
+// Set how camera rotates, pans and zooms around a target
 cfg.orbitCtrls.enable = true;
 cfg.orbitCtrls.autoRotate = true;
 cfg.orbitCtrls.maxDistance = 10;
 cfg.orbitCtrls.minDistance = 2;
 
-// Configures GLTF/GLB model loading.
+// Load GLTF/GLB model
 cfg.model.file = 'scene/argus_camera.glb';
 
-// Configures the scene environment map.
+// Set scene environment map
 cfg.envCtrls.enable = true;
 cfg.envCtrls.map = 'scene/brown_photostudio_02_1k.exr';
 
-// Configures the rendering backend.
+// Set rendering backend
 cfg.render.api = 'webgl';
 cfg.render.webgl.parameters.antialias = true;
 
 document.addEventListener('DOMContentLoaded', () => {
+  // Run illutions engine
   App.run(cfg);
-});
+}); 
